1c9b3d801e0e881fa9b4d698e9e880558467d5b8
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-09 11:16:20 +0100

    made some more changes

--- src/main.js
+++ src/main.js
@@ -16,7 +16,23 @@ const store = createStore({
         },
         increase(state, payload) {
             state.counter = state.counter + payload.value
-        }
+        },
+    
+    },
+    getters: {
+         finalCounter(state) {
+             return state.counter * 2;
+         },
+         normalizedCounter(state) {
+             const finalCounter = state.counter * 3;
+             if(finalCounter < 0) {
+                 return 0;
+             }
+             if(finalCounter > 100){
+                 return 100
+             }
+             return finalCounter
+         }
     }
 })
 
