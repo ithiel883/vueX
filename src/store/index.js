843a0a4b624d0d8e14165fdf0c23f5b6ede65c51
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-11 20:22:07 +0100

    final form

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from 'vuex';
 import rootMutations from './mutations.js'
 import rootActions from './actions.js'
 import rootGetters from './getters.js'
+import counterModule from './counter/index.js'
 const store = createStore({
     modules: {
         numbers: counterModule
