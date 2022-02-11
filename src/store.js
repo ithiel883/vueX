07b4527acca20c26b9488948c2ddd82baab1aa0b
ithiel883 <ajuka.zephiniah@gmail.com>
2022-02-11 20:07:19 +0100

    more structuring of code

--- src/store.js
+++ src/store.js
@@ -1,4 +1,3 @@
-import { createStore } from 'vuex';
 
 
 
@@ -46,34 +45,5 @@ const counterModule = {
     },
   };
 
-  
-  const store = createStore({
-      modules: {
-          numbers: counterModule
-      },
-    state() {
-      return {
-        isLoggedIn: false,
-      };
-    },
-    mutations: {
-      setAuth(state, payload) {
-        state.isLoggedIn = payload.isAuth;
-      },
-    },
-    actions: {
-      login(context) {
-        context.commit('setAuth', { isAuth: true });
-      },
-      logout(context) {
-        context.commit('setAuth', { isAuth: false });
-      },
-    },
-    getters: {
-      userIsAuthenticated(state) {
-        return state.isLoggedIn;
-      },
-    },
-  });
 
-export default store;
+  
---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from 'vuex';
+
+import rootMutations from './mutations.js'
+import rootActions from './actions.js'
+import rootGetters from './getters.js'
+const store = createStore({
+    modules: {
+        numbers: counterModule
+    },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: rootMutations,
+  actions: rootActions,
+  getters: rootGetters
+});
+
+export default store;
